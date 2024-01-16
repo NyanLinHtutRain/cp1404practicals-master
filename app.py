7a7899bcdcbe46f1e64f2a0497d8a7d893083721
Nyan Lin Htut <151938230+NyanLinHtutRain@users.noreply.github.com>
2024-01-17 03:20:23 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+#
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return '<h1>Hello World :)</h1>'
+#
+#
+# @app.route('/greet')
+# @app.route('/greet/<name>')
+# def greet(name=""):
+#     return f"Hello {name}"
+#
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+@app.route('/f/<float:celsius>')
+def convert_celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    return f'{fahrenheit:.2f}'
+
+
+if __name__ == '__main__':
+    app.run()
